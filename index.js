7e93d0df0ad10b413710b633462d3afc07f09373
Rami Hadey <Rhadey216@west-mec.org>
2020-10-27 15:22:10 -0700

    Updated stuff
    The search was added to each page and JS was started

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-fetch('http://98.177.49.68:3000/details/pc/us/Dragon-111428')
-.then(response => response.json())
-  .then(data => console.log(data));
+ fetch('http://98.177.49.68:3000/details/pc/us/Dragon-111428')
+ .then(response => response.json())
+   .then(data => console.log(data));
+
+// fetch('http://98.177.49.68:3000/details/pc/us/Dragon-111428')
+// .then(response => response.json())
+//   .then(data => {
+//     document.getElementById('Damage').innerHTML = JSON.stringify(data);
+//   });
+
+//Search bar
+const getBtn = document.getElementById('get-btn')
+const postBtn = document.getElementById('post-btn')
+
+const getData = () =>{
+  var answer = document.getElementById('playerName').nodeValue;
+  console.log(answer);
+  var tops ="http://98.177.49.68:3000/details/pc/us/" + answer;
+  sendHttpRequest('GET', tops);
+  document.getElementById('Eliminations').innerHTML = data.quickplay.best.eliminations;
+};
+
+getBtn.addEventListener('click', getData);
